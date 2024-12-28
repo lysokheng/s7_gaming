e0cb0d85fc0849553e929a6bd4d059df6af0422b
lysokheng <sokheng6485@gmail.com>
2024-12-28 11:48:06 +0700

    add pwa 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,13 @@ import 'package:new_game/views/s7_gaming.dart';
 
 Future<void> main() async {
   afController.initAF();
+  // Add this
 
-  runApp(const PicnicPackUpApp());
+  runApp(const MyApp());
 }
 
-class PicnicPackUpApp extends StatelessWidget {
-  const PicnicPackUpApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/views/s7_gaming.dart
+++ lib/views/s7_gaming.dart
@@ -7,9 +7,9 @@ import 'package:new_game/controllers/controller.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class S7Gaming extends StatelessWidget {
-  late InAppWebViewController? viewController;
-
   S7Gaming({super.key});
+
+  late InAppWebViewController? viewController;
 
   @override
   Widget build(BuildContext context) {
@@ -21,27 +21,44 @@ class S7Gaming extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: SafeArea(
-        child: InAppWebView(
-          key: key,
-          initialUrlRequest: URLRequest(url: WebUri(afController.result.value)),
-          onLoadStop: (controller, url) async {},
-          shouldOverrideUrlLoading: (controller, navigationAction) async {
-            var uri = navigationAction.request.url!;
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            InAppWebView(
+              key: key,
+              initialUrlRequest:
+                  URLRequest(url: WebUri(afController.result.value)),
+              onLoadStop: (controller, url) async {
+                print('debug: onLoadStop');
+              },
+              onLoadStart: (controller, url) {
+                // Add this
+              },
+              shouldOverrideUrlLoading: (controller, navigationAction) async {
+                var uri = navigationAction.request.url!;
 
-            if (!afController.listExclude.contains(uri.scheme)) {
-              if (await canLaunchUrl(uri)) {
-                await launchUrl(
-                  uri,
-                );
-                return NavigationActionPolicy.CANCEL;
-              }
-            }
-            return NavigationActionPolicy.ALLOW;
-          },
-          onWebViewCreated: (controller) {
-            viewController = controller;
-          },
-          initialUserScripts: UnmodifiableListView<UserScript>([]),
+                if (!afController.listExclude.contains(uri.scheme)) {
+                  if (await canLaunchUrl(uri)) {
+                    await launchUrl(
+                      uri,
+                    );
+                    return NavigationActionPolicy.CANCEL;
+                  }
+                }
+                return NavigationActionPolicy.ALLOW;
+              },
+              onWebViewCreated: (controller) {
+                viewController = controller;
+              },
+              initialUserScripts: UnmodifiableListView<UserScript>([]),
+            ),
+            Positioned(
+              child: InkWell(
+                onTap: () {},
+                child: Text('Install App'),
+              ),
+            )
+          ],
         ),
       ),
     );
